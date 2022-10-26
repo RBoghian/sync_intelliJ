01b2a8abf75efd3aa1775f380011de7961e10ea2
RBoghian <boghian_razvan@yahoo.com>
2022-10-26 18:59:42 +0300

    Test

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {0,1,2,3,4,5,6,7,8,9};
+
+        Random random = new Random();
+
+        for(int i = 0; i< arr.length; i++){
+            int tempToSwap = random.nextInt(arr.length);
+            int temp = arr[i];
+            arr[i] = arr[tempToSwap];
+            arr[tempToSwap] = temp;
+        }
+        for(int i = 0; i< arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
